7119b44829f94dd1021d5fa9c8dd4b8e13edecf7
LucasRoncheti <lucasroncheti@gmail.com>
2022-09-07 09:34:31 -0300

    first version

---
+++ js/Data.js
@@ -0,0 +1,37 @@
+let shopItemsData = [
+{   img:"images/ice_cream.svg",
+    id:"ac01",
+    name: "Açaí Cremoso",
+    size: ["300ml","500ml","700ml","1L", "Barca"],
+    priceSize: [10,12,15,20,25],
+    fruits:["Banana","Manga","Kiwi","Morango"],
+    complements:["Leite em pó","farofa de paçoca","M&M","Granola","Chocoball","Ovomaltine"],
+    topping:["Chocolate","Morango ","Leite Condensado"],
+    Addition:["Nutela","Doce de leite","Tortuguita", "Paçoca",],
+    priceAddition:[3,3,2,1],
+    
+
+},
+{   img:"images/ice_cream.svg",
+    id:"at01",
+    name: "Açaí Tradicional",
+    size: ["300ml","500ml","700ml","1L", "Barca"],
+    priceSize: [12,14,17,22,27],
+    fruits:["Banana","Manga","Kiwi","Morango"],
+    complements:["Leite em pó","farofa de paçoca","M&M","Granola","Chocoball","Ovomaltine"],
+    topping:["Chocolate","Morango ","Leite Condensado"],
+    Addition:["Nutela","Doce de leite","Tortuguita", "Paçoca",],
+    priceAddition:[3,3,2,1],
+
+},
+
+]
+
+let HotDogItems = [
+    { 
+        img:"images/burguer.svg",
+        name:"Tradicional",
+        desc:"",
+        price:10
+    },
+]
